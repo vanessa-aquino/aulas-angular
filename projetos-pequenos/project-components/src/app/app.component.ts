2055ec009ae566b70ae67ff2aa284e8e29d59ae6
Vanessa <vanessaquinoo@hotmail.com>
2024-12-20 16:52:11 -0300

    feat: finalizado a tela de compra com os dados de cartao de credito

--- projetos-pequenos/project-components/src/app/app.component.ts
+++ projetos-pequenos/project-components/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CreditCardComponent } from './credit-card/credit-card.component';
 import { PaymentComponent } from './payment/payment.component';
 import { PaymentWaysComponent } from './payment-ways/payment-ways.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CreditCardComponent, PaymentComponent, PaymentWaysComponent],
+  imports: [RouterOutlet, CreditCardComponent, PaymentComponent, PaymentWaysComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- projetos-pequenos/project-components/src/app/payment/payment.component.ts
+++ projetos-pequenos/project-components/src/app/payment/payment.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PaymentWaysComponent } from "../payment-ways/payment-ways.component";
 import { CreditCardComponent } from "../credit-card/credit-card.component";
+import { PaymentOption } from '../models/payment-option.interface';
+import { ParcelOption } from '../models/parcel-option.interface';
 
 @Component({
   selector: 'app-payment',
@@ -8,6 +10,30 @@ import { CreditCardComponent } from "../credit-card/credit-card.component";
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
+
+  totalValueStr: string = 'R$';
+  @Input() totalValue: number = 0;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.formatTotalValue();
+  }
+
+  formatTotalValue() {
+    this.totalValueStr = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(this.totalValue);
+  }
+
+  updatePaymentWay(paymentOption: PaymentOption): void {
+    console.log('Componente pai -', paymentOption)
+  }
+
+  updateParcel(parcelOption: ParcelOption): void {
+    console.log('Componente pai -', parcelOption)
+  }
+
 
 }
